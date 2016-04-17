92a307c10dec1751d8c797de7157e01b525c3af7
c14006078 <c14006078@mail.ncku.edu.tw>
2016-04-17 16:09:45 +0800

    Use kill to send the signal to child process

--- signal.h/kill_sig.c
+++ signal.h/kill_sig.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <signal.h>
+#include <assert.h>
+
+void prt_sig( int sig)
+{
+  if (sig == SIGINT)
+    printf("ptr_sig:I got the inrerrupt\n");
+  else
+    printf("I can't read the sig %d\n",sig);
+}
 
 int main()
 {
-  int kill
+  pid_t pd;
+  pd = fork();
+
+  if( pd > 0)
+  {
+    printf("I am Parent I will interrupt the child process\n");
+    sleep(1);
+    if( kill( pd, SIGINT) < 0)
+      assert("kill function return error");
+  }
+  else if( pd == 0)
+  {
+    signal( SIGINT, prt_sig);
+    printf("I am Child I going to infinite loop\n");
+    while(1) ;
+  }
+}
+    
