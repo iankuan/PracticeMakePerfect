dd4bc553bafa2e1dd706d86d79ffc7658d9f361a
c14006078 <c14006078@mail.ncku.edu.tw>
2016-05-12 22:39:14 +0800

    Fix the ENV(.bashrc) error for ld to find lib

--- script/opencv_2.4.12_install.sh
+++ script/opencv_2.4.12_install.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
 #Pre-Install
-apt-get install build-essential cmake git libgtk2.0-dev pkg-config\
+sudo pt-get install build-essential cmake git libgtk2.0-dev pkg-config\
   libavcodec-dev libavformat-dev libswscale-dev libtbb2 libtbb-dev\
   libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev\
   cmake-curses-gui g++ libavutil-dev libv4l-dev libeigen3-dev\
   libglew1.6-dev libgtk2.0-dev
-apt-get update
+sudo apt-get update
 
 #opencv-2.4.12 install
 cd ~/Downloads
 wget https://github.com/Itseez/opencv/archive/2.4.12.zip
 unzip opencv-2.4.12.zip
-cp opencv-2.4.12 ~
+cp -af opencv-2.4.12 ~
 cd ~/opencv-2.4.12
 mkdir release
 cd release
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
-      -D CMAKE_INSTALL_PREFIX=/usr/local \
       -D INSTALL_C_EXAMPLE=ON \
       ..
 make
@@ -25,5 +24,5 @@ make -j4 install
 
 #env config
 echo "# Use OpenCV and other custom-built libraries." >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> ~/.bashrc
 source ~/.bashrc
