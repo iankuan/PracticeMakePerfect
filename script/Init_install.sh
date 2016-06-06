b04185de85347f8deac4f3785c4c5314d1deca78
c14006078 <c14006078@mail.ncku.edu.tw>
2016-06-07 01:29:47 +0800

    add init install config

---
+++ script/Init_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#Aurtor: 
+#	Yen-Kwan Wu
+#
+#Program:
+#	Install the package I need it
+#
+#History:
+#	first release	2016/05/01
+
+sudo apt-get update
+
+
+sudo apt-get install vim build-essential linux-tools-common linux-tools-generic\
+	astyle colordiff gnuplot-x11 git apache2-utils check graphviz imagemagick\
+	doxygen cmake
+
+sudo apt-get update
