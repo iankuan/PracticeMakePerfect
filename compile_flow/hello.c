de3403f6d36d7a1b129f18a67b4f4f48464d8109
c14006078 <c14006078@mail.ncku.edu.tw>
2016-07-15 10:38:51 +0800

    1. Preprocessing 2. Compilation 3. Assembly 4.Linking( error)

---
+++ compile_flow/hello.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+int main() { printf("Hello ha~!"); return 0;}
---
+++ compile_flow/hello.s
@@ -0,0 +1,27 @@
+	.file	"hello.c"
+	.section	.rodata
+.LC0:
+	.string	"Hello ha~!"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.9.3-8ubuntu2~14.04) 4.9.3"
+	.section	.note.GNU-stack,"",@progbits
