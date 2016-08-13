550b1acc8ca272343bf88ecb21607db86527b2ba
c14006078 <c14006078@mail.ncku.edu.tw>
2016-08-13 14:50:14 +0800

    Variadic Macro: More test with no arguments ...

--- macro/variadic_macro.c
+++ macro/variadic_macro.c
@@ -6,9 +6,12 @@
  *	It should compile with C99
  */
 #define dprintf(...) printf(__VA_ARGS__);
+#define vprintf( arg, ...) fprintf( arg,__VA_ARGS__);
 
 int main()
 {
 	int i = 1;
+	dprintf("Without Any Argument\n");
 	dprintf("i = %d\n", i);
+	vprintf( stderr, "i = %d\n", i);
 }
