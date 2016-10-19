9bf92ecb050da9d308fa5bd06e189003e03ce2c9
c14006078 <c14006078@mail.ncku.edu.tw>
2016-10-19 22:36:35 +0800

    C11 _Atomic experience with muti-thread counter.

---
+++ c11_atomic/counter_atomic.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdatomic.h>
+#include <pthread.h>
+#include <assert.h>
+
+#define THREAD_COUNT 10000
+
+void add_one(void* d)
+{
+	_Atomic int *i = (_Atomic int *) d;
+	(*i)++;
+	printf("counter = %d\n", *i);
+}
+
+int main(int argc, char* argv[])
+{
+	_Atomic int counter = ATOMIC_VAR_INIT(0);
+
+	//_Atomic int _Atomic *counter;
+	//*counter = ATOMIC_VAR_INIT(0); FIXME:SEG FAULT
+	//*counter = 0;FIXME:SEG FAULT
+
+	//_Atomic int* pi; // pointer to atomic int
+	//int* _Atomic pi; // atomic pointer (to int)
+
+	pthread_t tid[THREAD_COUNT];
+
+	for(int i = 0; i < THREAD_COUNT; i++)
+		pthread_create(tid + i, NULL, (void *) &add_one, (void *) &counter);
+
+	for(int i = 0; i < THREAD_COUNT; i++)
+		pthread_join(tid[i], NULL);
+
+  assert(counter == THREAD_COUNT);
+	return 0;
+}
