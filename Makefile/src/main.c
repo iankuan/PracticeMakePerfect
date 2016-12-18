19c46f7a4714ed158aeeb6542fc8b18824ba4483
c14006078 <c14006078@gmail.com>
2016-12-18 17:59:58 +0800

    Use `vpath` to add file to searching list

---
+++ Makefile/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "myfunc.h"
+
+int main(int argc, char **argv)
+{
+    printf("I am main.c~\n");
+
+    myfunc();
+}
---
+++ Makefile/src/myfunc.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "myfunc.h"
+
+void myfunc(void)
+{
+    printf("I am myfunc()\n");
+}
