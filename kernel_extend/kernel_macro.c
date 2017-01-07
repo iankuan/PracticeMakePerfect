e420350a7d20a1bc57f5486432debdab7b61a8e1
c14006078 <c14006078@gmail.com>
2017-01-07 19:02:06 +0800

    Check the `zero-array` is worked for variable lengh array

--- kernel_extend/kernel_macro.c
+++ kernel_extend/kernel_macro.c
@@ -20,9 +20,16 @@ typedef struct _test {
 
 int main(int argc, char** argv)
 {
-    test* t = (test *) malloc(sizeof(test));
+    test* t = (test *) malloc(sizeof(test) + 10);
 
     WRITE_ONCE(t->num, 100);
+    WRITE_ONCE(*(char *)(t +  gcc_offsetof(test, data) + 1), 'h');
+    *(char *)(t +  gcc_offsetof(test, data) + 1) = 'h';
+    t->data[0] = 'h';
+    t->data[9] = 'e';
+    printf("check the data[0] = %c\n", t->data[0]);
+    printf("check the data[9] = %c\n", t->data[9]);
+
     //t->num = 100;
     //t->num;
     printf("t->num = %d\n", READ_ONCE(t->num));
@@ -31,6 +38,7 @@ int main(int argc, char** argv)
     printf("offsetof(test, arr) = %d\n",offsetof(test, arr));
     printf("offsetof(test, data) = %d\n",offsetof(test, data));
     printf("gcc_offsetof(test, data) = %d\n",gcc_offsetof(test, data));
+    printf("sizeof(t) = %d\n", sizeof(*t));
 
     test* tt = container_of(&t->data, test, data);
     printf("tt == t => %c\n", t == tt? 'T': 'F');
