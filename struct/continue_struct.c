ae4de0bdd89694d4fbe827e04ab3916406fc66de
c14006078 <c14006078@gmail.com>
2017-01-10 20:44:03 +0800

    Test Continue Space Structure contain String, but failed... A few mistake I maade such as Varible Size struct can't deinitiallize and assign var.str[len] with "string"(not assignable after declaration).

---
+++ struct/continue_struct.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+struct _st {
+    char *str;
+    int     a;
+    int     b;
+};
+
+#define init_struct(var, _str, _a, _b) \
+    struct _st var = { \
+        .str = _str, \
+        .a   = _a, \
+        .b   = _b, \
+    };
+
+#define init_struct1(var, _str, _a, _b) \
+    struct _st1 { \
+        char str[strlen(_str) + 1]; \
+        int     a; \
+        int     b; \
+    }; \
+    
+/*    struct _st1 var; \
+    var.str = _str; \///FIXME: var.str is not assignable...
+    var.a = _a;\
+    var.b = _b;*/
+
+    ///FIXME: error: variable-sized object may not be initialized
+    /*struct _st1 var = { \
+        .str = _str, \
+        .a   = _a, \
+        .b   = _b, \
+    };*/
+
+int main() {
+
+    init_struct(st, "This is str", 10, 20);
+
+    init_struct1(st1, "This is str", 10, 20);
+
+    printf("Use GDB\n");
+
+    return 0;
+}
