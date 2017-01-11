f4ece6a561813f9b4ea4d8c5d6fcb5a26efef0ba
c14006078 <c14006078@gmail.com>
2017-01-11 13:58:17 +0800

    Test Varidic Macro for building felixible function.

--- macro/variadic_macro.c
+++ macro/variadic_macro.c
@@ -6,12 +6,22 @@
  *	It should compile with C99
  */
 #define dprintf(...) printf(__VA_ARGS__);
+#define dprintf1(str,...) printf(str __VA_ARGS__);
+#define dprintf1(str,...) printf(str __VA_ARGS__);
 #define vprintf( arg, ...) fprintf( arg,__VA_ARGS__);
+
+#define func(num,...) __VA_ARGS__(num)
+
+int db(int i) { return i * 2;}
 
 int main()
 {
 	int i = 1;
 	dprintf("Without Any Argument\n");
 	dprintf("i = %d\n", i);
+	dprintf1("only str no var args\n");
+
+	printf("test func(num,...) = %d\n", func(10, db));
+
 	vprintf( stderr, "i = %d\n", i);
 }
