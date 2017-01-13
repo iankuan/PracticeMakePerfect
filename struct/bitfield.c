de29cd1069a6c816e1db922f7a255ee78966c928
c14006078 <c14006078@gmail.com>
2017-01-13 23:05:15 +0800

    Check the extension in bitfield in struct. Signed one would be signed extension. Unsigned one would be zero extension.

--- struct/bitfield.c
+++ struct/bitfield.c
@@ -7,13 +7,25 @@ typedef struct _test {
     int a14_15 :2;
     int a16_31: 16;
 } test_t;
-    
+
+ typedef struct _test1 {
+    unsigned int a0_12 :13;
+    unsigned int a13 :1;
+    unsigned int a14_15 :2;
+    unsigned int a16_31: 16;
+} test1_t;
+   
 
 int main() {
 
     test_t a = {.a0_12 = 13, .a13 = 1, .a14_15 = 2, .a16_31 = 16};
+    test1_t b = {.a0_12 = 13, .a13 = 1, .a14_15 = 2, .a16_31 = 16};
 
-    printf("(bitfiled would signed extension with arithmetic)a0_12 = %d, a13 = %d, a14_15 = %d, a16_31 = %d\n", a.a0_12, a.a13, a.a14_15, a.a16_31);
-    printf("(bitfiled would signed extension with arithmetic)a0_12 = %u, a13 = %u, a14_15 = %u, a16_31 = %u\n", a.a0_12, a.a13, a.a14_15, a.a16_31);
-    printf("(bitfiled would signed extension with arithmetic)a0_12 =0x%X, a13 =0x%X, a14_15 =0x%X, a16_31 =0x%X\n", a.a0_12, a.a13, a.a14_15, a.a16_31);
+    printf("a(signed):(bitfiled would signed extension with arithmetic)a0_12 = %d, a13 = %d, a14_15 = %d, a16_31 = %d\n", a.a0_12, a.a13, a.a14_15, a.a16_31);
+    printf("a(signed):(bitfiled would signed extension with arithmetic)a0_12 = %u, a13 = %u, a14_15 = %u, a16_31 = %u\n", a.a0_12, a.a13, a.a14_15, a.a16_31);
+    printf("a(signed):(bitfiled would signed extension with arithmetic)a0_12 =0x%X, a13 =0x%X, a14_15 =0x%X, a16_31 =0x%X\n\n", a.a0_12, a.a13, a.a14_15, a.a16_31);
+    
+     printf("b(unsigned):(bitfiled would extend with zero)a0_12 = %d, a13 = %d, a14_15 = %d, a16_31 = %d\n", b.a0_12, b.a13, b.a14_15, b.a16_31);
+    printf("b(unsigned):(bitfiled would extend with zero)a0_12 = %u, a13 = %u, a14_15 = %u, a16_31 = %u\n", b.a0_12, b.a13, b.a14_15, b.a16_31);
+   printf("b(unsigned):(bitfiled would extend with zero)a0_12 =0x%X, a13 =0x%X, a14_15 =0x%X, a16_31 =0x%X\n", b.a0_12, b.a13, b.a14_15, b.a16_31);
 }
