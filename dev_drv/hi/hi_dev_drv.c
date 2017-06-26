972a7c8816949e3f36717ae90bc1a8eef0dbd6cf
yenWu <c14006078@gmail.com>
2017-06-26 16:26:04 +0800

    [Driver] hi: section decalaration and module info

--- dev_drv/hi/hi_dev_drv.c
+++ dev_drv/hi/hi_dev_drv.c
@@ -1,17 +1,30 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-MODULE_LICENSE("Can I put anything here without warning?");
+MODULE_LICENSE("Dual BSD/GPL"); //GPL, GPL v2 ...
 
-static int hi_init(void)
+MODULE_AUTHOR("Yen-Kuan Wu");
+MODULE_DESCRIPTION("Implement a simple module to show \"Hi Device Driver\"");
+//MODULE_VERSION("");
+//MODULE_ALIAS("");
+//MODULE_DEVICE_TABLE("");
+
+//__devinitdata
+int __initdata disappear_after_init = 666;
+
+//__devinit: for hotpluggable devices
+static int __init hi_init(void)
 {
-    printk(KERN_ALERT "Hi Device Driver\n");
+    printk(KERN_ALERT "Hi Device Driver %d\n", disappear_after_init);
     return 0;
 }
+//EXPORT_SYMBOL(hi_init);
+//EXPORT_GPL_SYMBOL(hi_init);
 
-static void hi_exit(void)
+static void __exit hi_exit(void)
 {
     printk(KERN_ALERT "Bye Device Driver\n");
+    //printk(KERN_ALERT "Bye Device Driver %d\n", disappear_after_init);
 }
 
 module_init(hi_init);
