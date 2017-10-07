a18fe52b8220592547ef5eec72d8194815b73392
Yen-Kuan Wu <c14006078@gmail.com>
2017-10-07 21:20:48 +0800

    gcc ext: ES must be within func without static

--- gcc_extension/cvt_macro2func.c
+++ gcc_extension/cvt_macro2func.c
@@ -7,25 +7,44 @@
  *      2. statement expressions
  */
 
+#define WRAP(macro)                    \
+            ({                         \
+            void __null__(void) {macro}\
+            __null__;                  \
+            })
+
+#define function() printf("Hello World\n"); 
+
 struct obj {
-    void (*func)(void);
+    void (*func0)(void);
+    void (*func1)(void);
 };
+
+// FIXME: Didn't work ...
+/* void (*func3)(void) = WRAP(function()); */
+/* static void (*func3)(void) = WRAP(function()) */
+//
 
 int main(void)
 {
-    struct obj objs = {
-        .func = ({
-            void __null__(void) {printf("Hello World\n");}
-            __null__;
-            }),
+    void (*func3)(void) = WRAP(function());
+
+    /* Didn't work, too */
+    //static struct obj objs[3] = {
+
+    struct obj objs[3] = {
+        [2] = {
+            .func0 = WRAP(function()),
+            .func1 = WRAP(function()),
+        },
     };
-    /* 
+   /* 
     func = ({
             void __null__(void) {printf("Hello World\n");};// No matter append `;` at the end of func, all work
             __null__;
             });
     */
 
-
-    objs.func();
+    objs[2].func0();
+    objs[2].func1();
 }
